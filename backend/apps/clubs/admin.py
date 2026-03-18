17061ba3ff1bd16bdff88426ee0de258db71eba9
Gleb Ushakov <101178703+ushakovgleb@users.noreply.github.com>
2026-03-18 21:11:47 +0300

    feat(clubs): добавлена регистрация моделей Club и Room в Django Admin

--- backend/apps/clubs/admin.py
+++ backend/apps/clubs/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Club, Room
+
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display  = ['id', 'name', 'phone', 'timezone']
+    search_fields = ['name', 'address', 'phone']
+    ordering      = ['-id']
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display  = ['id', 'name', 'kind', 'capacity', 'club']
+    list_filter   = ['kind', 'club']
+    search_fields = ['name', 'club__name']
+    ordering      = ['-id']
